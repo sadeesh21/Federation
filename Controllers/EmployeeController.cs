b5050fea7cdb014ae70364056d4e17a418ecf057
Sadeesh Kanna <38082578+sadeesh21@users.noreply.github.com>
2022-04-08 12:05:31 +0530

    Add files via upload

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,33 @@
+﻿using Federation.Models;
+using Federation.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Federation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly FerderationDbContext _context;
+        private readonly IEmployees _employeeRespository;
+
+        public EmployeeController(IEmployees employee, FerderationDbContext context)
+        {
+            _context = context;
+            _employeeRespository = employee;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = _employeeRespository.GetAllEmployee();
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+    }
+}
