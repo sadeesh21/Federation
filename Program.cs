7f63ecf332ae9e6a630370e0ae77405994925269
Sadeesh Kanna <38082578+sadeesh21@users.noreply.github.com>
2022-04-08 12:06:29 +0530

    Add files via upload
    Add Phase 1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
+using Federation.Models;
+using Federation.Models.Data;
+using Federation.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//builder.Services.AddDbContext<FerderationContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+
+string connString = builder.Configuration.GetConnectionString("FedConnection");
+builder.Services.AddDbContext<FerderationDbContext>(o =>
+{
+    o.UseSqlServer(connString);
+});
+//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<IEmployees, EmployeesRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,10 +29,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
