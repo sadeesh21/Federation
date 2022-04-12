21d43cff17445ce1584c27add67e66b7d110e120
Sadeesh Kanna <38082578+sadeesh21@users.noreply.github.com>
2022-04-12 12:16:12 +0530

    CRUD

---
+++ Repository/EmployeesRepository.cs
@@ -0,0 +1,51 @@
+﻿using Federation.Models;
+using Federation.Models.Data;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+namespace Federation.Repository
+{
+    public class EmployeesRepository : IEmployees
+    {
+        FerderationDbContext _context;
+        public EmployeesRepository(FerderationDbContext context)
+        {
+            _context = context;
+        }
+        public Employees Create(Employees objEmployee)
+        {
+            _context.Employees.Add(objEmployee);
+            _context.SaveChanges();
+            return objEmployee;
+        }
+        public async Task<Employees> GetEmployee(int employeeId)
+        {
+            return await _context.Employees.FirstOrDefaultAsync(e => e.EmployeeId == employeeId);
+        }
+        public async Task<Employees> DeleteEmployee(int employeeId)
+        {
+            var result = await _context.Employees
+                .FirstOrDefaultAsync(e => e.EmployeeId == employeeId);
+            if (result != null)
+            {
+                _context.Employees.Remove(result);
+                await _context.SaveChangesAsync();
+                return result;
+            }
+
+            return null;
+        }
+       
+        public async Task<IEnumerable<Employees>> GetEmployees()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        public Employees Update(Employees objEmployee)
+        {
+            _context.Employees.Update(objEmployee);
+            // await
+            var obj = _context.SaveChanges();
+            return objEmployee;
+        }
+    }
+}
---
+++ Repository/IEmployees.cs
@@ -0,0 +1,14 @@
+﻿using Federation.Models.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Federation.Repository
+{
+    public interface IEmployees
+    {
+        Task<Employees> GetEmployee(int employeeId);
+        Employees Create(Employees objEmployee);
+        Employees Update(Employees objEmployee);
+        Task<IEnumerable<Employees>> GetEmployees();
+        Task<Employees> DeleteEmployee(int employeeId);
+    }
+}
