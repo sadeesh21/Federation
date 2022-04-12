cc9e9511bfcc7f3413a7ba3c8b2728ffdfec89a4
Sadeesh Kanna <38082578+sadeesh21@users.noreply.github.com>
2022-04-12 11:52:11 +0530

    Add files via upload
    CRUD

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
 ﻿using Federation.Models;
+using Federation.Models.Data;
 using Federation.Repository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -19,15 +20,67 @@ namespace Federation.Controllers
         }
 
         [HttpGet]
-        public IActionResult Get()
+        public async Task<ActionResult> GetEmployees()
         {
-            var result = _employeeRespository.GetAllEmployee();
+            try
+            {
+                return Ok(await _employeeRespository.GetEmployees());
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
 
+        [HttpPost]
+        public IActionResult Post([FromBody] Employees objEmployees)
+        {
+            var result = _employeeRespository.Create(objEmployees);
             if (result == null)
             {
                 return NotFound();
             }
             return Ok(result);
         }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Employees objEmployees)
+        {
+            var result = _employeeRespository.Update(objEmployees);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<Employees>> DeleteEmployee(int id)
+        {
+            try
+            {
+                var employeeToDelete = await _employeeRespository.GetEmployee(id);
+
+                if (employeeToDelete == null)
+                {
+                    return NotFound($"Employee with Id = {id} not found");
+                }
+
+                return await _employeeRespository.DeleteEmployee(id);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error deleting data");
+            }
+        }
+
+        //[HttpGet]
+        //public async Task<IActionResult> GetAsync()
+        //{
+        //    var result = await _employeeRespository.GetAllEmployee();
+        //    return Ok(result);
+        //}
     }
 }
